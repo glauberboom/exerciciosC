8e28e664e3579b12e6bcc51e286b247e50de8265
glauberboom <glauberboom@hotmail.com>
2019-07-26 13:36:17 -0300

    Add files via upload
    Change-Id: I40cadca22e311da9dddf62cc1a7c303a5268f195
    Signed-off-by: glauber <glauber@pc>

---
+++ aula05.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : aula05.c
+ Author      : Glauber
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+
+
+ glauber
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
